2b7938c6c3127a059eac9b40b5b2b084f6505e18
Core <seorisachs@gmail.com>
2015-02-04 12:49:04 -0500

    started file

---
+++ 1.28/Assets/Scripts/SafeZoneMove.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SafeZoneMove : MonoBehaviour {
+
+	public float noiseFreq = 1;
+	public float noiseAmp = 1;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float moveX = (Mathf.PerlinNoise(Time.realtimeSinceStartup, 0) - .5f) * noiseAmp;
+		float moveY = (Mathf.PerlinNoise(0, Time.realtimeSinceStartup) - .5f) * noiseAmp;
+
+		transform.position = new Vector3 (moveX, moveY, 0); 
+	
+	}
+}
---
+++ 1.28/Assets/Scripts/SafetyDance.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SafetyDance : MonoBehaviour {
+
+	GameObject target;
+
+	// Use this for initialization
+	void Start () {
+		target = GameObject.Find("SafeZone");
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Vector3.Distance(transform.position, target.transform.position) > 3){
+			Destroy(gameObject);
+		}
+	}
+}
