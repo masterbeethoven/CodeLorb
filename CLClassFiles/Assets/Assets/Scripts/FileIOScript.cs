7eb500b5a6e39e68c78d40edcd1732f75dd401e5
Core <seorisachs@gmail.com>
2015-02-11 17:34:25 -0500

    IO Script

--- CLClassFiles/Assets/Assets/Scripts/FileIOScript.cs
+++ CLClassFiles/Assets/Assets/Scripts/FileIOScript.cs
@@ -8,7 +8,7 @@ public class FileIOScript : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 
-		//Opening up a stream to a file 
+		//Opening up a stream to a file to wrtie to
 		StreamWriter writer = new StreamWriter(FILE_NAME, false);
 
 		//Writing to that file 
@@ -16,6 +16,19 @@ public class FileIOScript : MonoBehaviour {
 
 		//Closing the stream to that file
 		writer.Close();
+
+		//opwn up a stream to file to read from
+		StreamReader reader = new StreamReader (FILE_NAME);
+
+		int content = reader.Read();
+
+		Debug.Log (content);
+
+		content=reader.Read();
+
+		Debug.Log (content);
+
+		reader.Close ();
 	}
 	
 	// Update is called once per frame
