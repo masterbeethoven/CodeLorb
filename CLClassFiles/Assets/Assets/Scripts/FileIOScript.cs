600af53d5374dd1249a5a402d0e9a0a281c839e8
Core <seorisachs@gmail.com>
2015-02-11 17:12:17 -0500

    name change? and system.io

---
+++ CLClassFiles/Assets/Assets/Scripts/FileIOScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class FileIOScript : MonoBehaviour {
+
+	const string FILE_NAME = "Week3Save.txt";
+	// Use this for initialization
+	void Start () {
+
+		//Opening up a stream to a file 
+		StreamWriter writer = new StreamWriter(FILE_NAME, false);
+
+		//Writing to that file 
+		writer.Write("Hello World!");
+
+		//Closing the stream to that file
+		writer.Close();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
