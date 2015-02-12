55229338f496de2fca653a56d47270a2cbdbd599
Core <seorisachs@gmail.com>
2015-02-12 14:07:47 -0500

    io and txt files

--- CLClassFiles/Assets/Assets/Scripts/FileIOScript.cs
+++ CLClassFiles/Assets/Assets/Scripts/FileIOScript.cs
@@ -5,14 +5,21 @@ using System.IO;
 public class FileIOScript : MonoBehaviour {
 
 	const string FILE_NAME = "Week3Save.txt";
+	const char DELIMITER = '|';
+
+	string name = "Matt"; 
+	int score = 1000; 
 	// Use this for initialization
 	void Start () {
 
+		//Vector3 vec = new Vector3(100,200,300);
+		//vectors are possible 
 		//Opening up a stream to a file to wrtie to
 		StreamWriter writer = new StreamWriter(FILE_NAME, false);
 
 		//Writing to that file 
-		writer.Write("Hello World!");
+		writer.Write(name + DELIMITER + score);
+		//2 string function, pass obj or primative into a string 
 
 		//Closing the stream to that file
 		writer.Close();
@@ -20,13 +27,16 @@ public class FileIOScript : MonoBehaviour {
 		//opwn up a stream to file to read from
 		StreamReader reader = new StreamReader (FILE_NAME);
 
-		int content = reader.Read();
+		string content = reader.ReadLine();
 
-		Debug.Log (content);
+		char[] delimiterChars ={'|'};
 
-		content=reader.Read();
+		string[] scoreSplit = content.Split(delimiterChars);
 
-		Debug.Log (content);
+		Debug.Log ("name: " + scoreSplit[0]);
+		Debug.Log ("score: " + scoreSplit[1]);
+
+		int saveScore = int.Parse(scoreSplit[1]);
 
 		reader.Close ();
 	}
