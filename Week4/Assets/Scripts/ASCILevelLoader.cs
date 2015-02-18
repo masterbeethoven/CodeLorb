6ab7073b5ccde057b4e84064b61e2694bec94921
Core <seorisachs@gmail.com>
2015-02-18 16:14:38 -0500

    Week 4 new
    bc things got messed up

---
+++ Week4/Assets/Scripts/ASCILevelLoader.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ASCILevelLoader : MonoBehaviour {
+
+	string row = "X---XXX"; 
+
+	// Use this for initialization
+	void Start () {
+
+		char[] rowArray = row.ToCharArray();
+
+		for (int i = 0; i < row.Length; i++){
+			char c = rowArray[i];
+
+			if(c == 'X'){
+				GameObject brick = Instantiate(Resources.Load("Brick")) as GameObject;
+				brick.transform.position = new Vector3(i * brick.transform.localScale.x,15,0);
+				//the .x is the X scale of the brick
+			}
+
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
