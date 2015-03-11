f6b87a67c8a3e09b034a20bd30793263a22ffb84
Core <seorisachs@gmail.com>
2015-03-11 17:15:34 -0400

    new week6 project, unity5 destroyed everything

---
+++ Week6/Assets/Scripts/PrintCubeInfo.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PrintCubeInfo : PrintGameObjectInfo {
+	//PrintGameObjectInfo IS A SUPERCLASS
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		PrintInfo();
+		//inheriting from PrintGameObjectInfo
+	
+	}
+
+	protected override string MakeInfoString(){
+		BoxCollider bc = GetComponent<BoxCollider>();
+
+		string cubeInfo = base.MakeInfoString() + 
+			"\nBC Size: " + bc.size+
+			"\nBC Materical: " +bc.material+
+				"\nBC Center: " + bc.center;
+			//"\nI AM A CUBE";
+
+		//return "I AM A CUBE";
+		return cubeInfo; 
+
+	}
+}
---
+++ Week6/Assets/Scripts/PrintGameObjectInfo.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PrintGameObjectInfo : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		PrintInfo();
+	
+	}
+
+	public void PrintInfo(){
+		Debug.Log (MakeInfoString());
+
+	}
+
+	protected virtual string MakeInfoString(){
+		string infoString;
+
+		infoString = "Position: " + transform.position.ToString() + 
+				"\nRotation: " + transform.rotation +
+				"\nScale: " + transform.localScale;
+
+
+	
+
+		return infoString;
+
+	}
+}
+
+//have to click on the console outputs to get that info
